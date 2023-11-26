5f8234ba22bb4f488229173347a52c18acc0360e
Mateusz-92 <matuesz.zasada92@gmail.com>
2023-11-26 13:28:00 +0100

    added images ( btn)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,13 +2,20 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
+import Logo from "@/components/common/header/logo";
+import Button from "@/components/common/button/btn";
+import { useRouter } from "next/router";
+import BottomBar from "@/components/bottom_bar/bottomBar";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <>
+      <Logo></Logo>
       <div>This is main page</div>
+      <BottomBar />
+      {/* <Button onNavigate={handler} src="images/home.png" alt="sss"></Button> */}
     </>
   );
 }
