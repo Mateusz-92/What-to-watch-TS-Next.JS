5d95eeb7aa7c88bd49cd899bf8a098b709394e2e
Mateusz-92 <matuesz.zasada92@gmail.com>
2023-11-26 13:27:15 +0100

    created BottomBar component

---
+++ src/components/bottom_bar/bottomBar.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import Button from "../common/button/btn";
+import { useRouter } from "next/router";
+
+const BottomBar: React.FC = () => {
+  const router = useRouter();
+  const navigateHandler = (path: string) => {
+    router.push(`/${path}`);
+  };
+
+  return (
+    <div>
+      <Button
+        onNavigate={navigateHandler}
+        src="images/home.png"
+        alt="home"
+        path=""
+      />
+      <Button
+        onNavigate={navigateHandler}
+        src="images/favourite.png"
+        alt="our favourite"
+        path="favourites"
+      />
+      <Button
+        onNavigate={navigateHandler}
+        src="images/funfact.png"
+        alt="funny facts"
+        path="funny_facts"
+      />
+      <Button
+        onNavigate={navigateHandler}
+        src="images/person.png"
+        alt="about
+        "
+        path="aobut"
+      />
+    </div>
+  );
+};
+
+export default BottomBar;
