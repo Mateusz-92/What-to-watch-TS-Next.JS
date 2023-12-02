02844314c19048527b3bc05d8a6ac8c4ff84ee68
Mateusz-92 <matuesz.zasada92@gmail.com>
2023-12-02 13:56:34 +0100

    changed styles and added alt prop

--- src/components/common/headers/CategoryHeader.tsx
+++ src/components/common/headers/CategoryHeader.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import styles from "./CategoryHeader.module.css";
 
-const CategoryHeader: React.FC<{ title: string; path: string }> = (props) => {
+const CategoryHeader: React.FC<{ title: string; path: string; alt: string }> = (
+  props
+) => {
   return (
     <div className={styles.container}>
       <h3>{props.title}</h3>
-      <div>
-        <img src={props.path}></img>
-      </div>
+      <img src={props.path} alt={props.alt}></img>
     </div>
   );
 };
