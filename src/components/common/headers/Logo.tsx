17133f1fdfb853b945ac016dafa029a1ab351c9e
Mateusz-92 <matuesz.zasada92@gmail.com>
2023-12-02 13:56:57 +0100

    chaged folder

---
+++ src/components/common/headers/Logo.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./logo.module.css";
+
+const Logo: React.FC = () => {
+  return (
+    <div>
+      <div className={styles.logo}>
+        <img src="/images/logo.png" alt="logo"></img>
+        <p>What to watch</p>
+      </div>
+    </div>
+  );
+};
+export default Logo;
