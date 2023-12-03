ab80bfd2759cd5567a6e7e094453446a7b0d712d
Mateusz-92 <matuesz.zasada92@gmail.com>
2023-12-03 13:59:35 +0100

    Added component with data

---
+++ src/components/movies_category/movies_genre/MovieYears.tsx
@@ -0,0 +1,67 @@
+import { getMovieByDecade } from "../../../pages/api/api";
+import React, { useState } from "react";
+
+import { MenuData } from "../../common/drop_down_menu/DropDown";
+
+import DropDownMenu from "@/components/common/drop_down_menu/DropDown";
+import MoviesCoverList from "@/components/movie_cover/movie_cover_list/MoviesCoverList";
+import CategoryHeader from "@/components/common/headers/CategoryHeader";
+
+const MovieYears: React.FC = (props) => {
+  const [selectedTag, setSelectedTag] = useState<string>("");
+
+  const yearsList: MenuData[] = [
+    {
+      listName: "1910",
+    },
+    {
+      listName: "1920",
+    },
+    {
+      listName: "1930",
+    },
+    {
+      listName: "1940",
+    },
+    {
+      listName: "1950",
+    },
+    {
+      listName: "1960",
+    },
+    {
+      listName: "1970",
+    },
+    {
+      listName: "1980",
+    },
+    {
+      listName: "1990",
+    },
+    {
+      listName: "2000",
+    },
+  ];
+
+  const changeTagHandler = (tag: string) => {
+    setSelectedTag(tag);
+  };
+  return (
+    <div>
+      <CategoryHeader
+        title="Lata produkcji"
+        path="images/clock.png"
+        alt="lata produkcji"
+      />
+      {yearsList?.length && (
+        <DropDownMenu
+          startTitle="Lista"
+          data={yearsList}
+          onSelectTag={changeTagHandler}
+        />
+      )}
+      <MoviesCoverList tag={selectedTag} fetch={getMovieByDecade} />
+    </div>
+  );
+};
+export default MovieYears;
