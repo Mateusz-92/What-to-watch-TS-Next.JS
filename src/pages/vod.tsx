ee47718478b3cc73c9e68041a820db0d35fc295f
Mateusz-92 <matuesz.zasada92@gmail.com>
2023-12-03 14:00:16 +0100

    added files vod and years to routing

---
+++ src/pages/vod.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import MovieStreamings from "@/components/movies_category/movies_genre/MovieStreamings";
+const Vod: React.FC = () => {
+  return <MovieStreamings />;
+};
+
+export default Vod;
---
+++ src/pages/year.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import MovieYears from "@/components/movies_category/movies_genre/MovieYears";
+
+const Years: React.FC = () => {
+  return <MovieYears />;
+};
+
+export default Years;
