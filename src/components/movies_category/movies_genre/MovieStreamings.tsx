31648e9fcbae5bab3c3234fc48e070df114be610
Mateusz-92 <matuesz.zasada92@gmail.com>
2023-12-03 13:59:26 +0100

    Added component with data

---
+++ src/components/movies_category/movies_genre/MovieStreamings.tsx
@@ -0,0 +1,70 @@
+import { getVodMovie } from "../../../pages/api/api";
+import React, { useState } from "react";
+
+import { MenuData } from "../../common/drop_down_menu/DropDown";
+
+import DropDownMenu from "@/components/common/drop_down_menu/DropDown";
+import MoviesCoverList from "@/components/movie_cover/movie_cover_list/MoviesCoverList";
+import CategoryHeader from "@/components/common/headers/CategoryHeader";
+
+const MovieStreamings: React.FC = (props) => {
+  const [selectedTag, setSelectedTag] = useState<string>("");
+
+  const vodServices: MenuData[] = [
+    {
+      listName: "35",
+    },
+    {
+      listName: "amazon",
+    },
+    {
+      listName: "chili",
+    },
+    {
+      listName: "cineman",
+    },
+    {
+      listName: "hbo",
+    },
+    {
+      listName: "kato",
+    },
+    {
+      listName: "netflix",
+    },
+    {
+      listName: "ninateka",
+    },
+    {
+      listName: "player",
+    },
+    {
+      listName: "tvp",
+    },
+    {
+      listName: "wtf",
+    },
+  ];
+
+  const changeTagHandler = (tag: string) => {
+    setSelectedTag(tag);
+  };
+  return (
+    <div>
+      <CategoryHeader
+        title="Platformy streamingowe"
+        path="images/vod.png"
+        alt="Platformy streamingowe"
+      />
+      {vodServices?.length && (
+        <DropDownMenu
+          startTitle="Lista"
+          data={vodServices}
+          onSelectTag={changeTagHandler}
+        />
+      )}
+      <MoviesCoverList tag={selectedTag} fetch={getVodMovie} />
+    </div>
+  );
+};
+export default MovieStreamings;
