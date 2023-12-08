c3dc6f9500b71bfb1850e0a8d687535914a2c5d6
Mateusz-92 <matuesz.zasada92@gmail.com>
2023-12-08 18:23:14 +0100

    implement RandomMovie component in path file

--- src/pages/random.tsx
+++ src/pages/random.tsx
@@ -1,44 +1,9 @@
+import RandomMovie from "@/components/movies_category/movies_genre/RandomMovie";
+import RadnomMovie from "@/components/movies_category/movies_genre/RandomMovie";
 import React from "react";
-import { useEffect, useState } from "react";
 
-import MovieDetails from "@/components/movie_details/MovieDetails";
-
-import { MovieData } from "./api/api";
-import { fetchRandomMovie } from "./api/api";
-const RadnomMovie: React.FC = () => {
-  const [movie, setMovie] = useState<MovieData>();
-
-  const fetchRandomMovieHandler = () => {
-    fetchRandomMovie().then((data) => {
-      setMovie(data);
-    });
-  };
-
-  useEffect(() => {
-    fetchRandomMovieHandler();
-  }, []);
-
-  return (
-    <div>
-      {movie && (
-        <>
-          <MovieDetails
-            title={movie.title}
-            alt={movie.title}
-            country={movie.country}
-            description={movie.description}
-            thumbnail={movie.thumbnail}
-            year={movie.year}
-            genres={movie.genres}
-            vod={movie.vod || []}
-            key={movie.id}
-          />
-          {/* bellow will be reusable button  */}
-          <button onClick={fetchRandomMovieHandler}>Losuj</button>
-        </>
-      )}
-    </div>
-  );
+const Random: React.FC = () => {
+  return <RandomMovie />;
 };
 
-export default RadnomMovie;
+export default Random;
