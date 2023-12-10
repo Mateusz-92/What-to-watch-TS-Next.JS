f84b07807742af5f5bb122086f71d2f1a972704e
Mateusz-92 <matuesz.zasada92@gmail.com>
2023-12-10 17:47:44 +0100

    added page route component

---
+++ src/pages/ourfavourites.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import MovieFavorite from "@/components/movies_category/movies_genre/MovieFavorites";
+
+const Ourfavourites: React.FC = () => {
+  return <MovieFavorite />;
+};
+export default Ourfavourites;
