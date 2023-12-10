9358f034ced27a5541c15d00a5e2e0fa665148b2
Mateusz-92 <matuesz.zasada92@gmail.com>
2023-12-10 17:47:09 +0100

    Added MovieFavorites component

---
+++ src/components/movies_category/movies_genre/MovieFavorites.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import MoviesCoverList from "@/components/movie_cover/movie_cover_list/MoviesCoverList";
+import CategoryHeader from "@/components/common/headers/CategoryHeader";
+import { getMovieByTag } from "@/pages/api/api";
+
+const MovieFavorite: React.FC = () => {
+  return (
+    <div>
+      <CategoryHeader
+        alt="favorite"
+        path="/images/recommended2.png"
+        title="Polecamy"
+      />
+      <MoviesCoverList fetch={getMovieByTag} tag="fav" />
+    </div>
+  );
+};
+export default MovieFavorite;
