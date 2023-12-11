ed1646d8a24d0192ed7f371ae95abb6c1d034821
Mateusz-92 <matuesz.zasada92@gmail.com>
2023-12-11 20:06:23 +0100

    added MovieFunFacts component

---
+++ src/components/movies_category/movies_genre/MovieFunFacts.tsx
@@ -0,0 +1,73 @@
+import React, { useEffect, useState, useRef } from "react";
+import MovieDetails from "@/components/movie_details/MovieDetails";
+import { FunFact } from "@/pages/api/api";
+import { fetchRandomFunFact } from "@/pages/api/api";
+import { useRouter } from "next/router";
+import Button from "@/components/common/buttons/button/Button";
+import MovieCover from "@/components/movie_cover/MovieCover";
+
+const MovieFunFacts: React.FC = () => {
+  const [movie, setMovie] = useState<FunFact | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const hasFetchedData = useRef(false);
+  const router = useRouter();
+
+  const fetchFanFactsHandler = () => {
+    setIsLoading(true);
+    fetchRandomFunFact().then((data) => {
+      setMovie(data);
+      setIsLoading(false);
+      console.log("fetch fun");
+    });
+  };
+  const moreNavigatorHandler = (link: string) => {
+    router.push(link);
+  };
+  const movieNavigatorHandler = (id: number) => {
+    router.push(`movie/${id}`);
+  };
+  useEffect(() => {
+    if (!hasFetchedData.current) {
+      fetchFanFactsHandler();
+      hasFetchedData.current = true;
+    }
+  }, []);
+
+  return (
+    <div>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        movie && (
+          <div className="tmp">
+            <MovieDetails
+              title={movie.title}
+              alt={movie.title}
+              description={movie.content}
+              path={movie.image}
+              key={movie.id}
+            />
+            <Button onClick={fetchFanFactsHandler} text="LOSUJ"></Button>
+            <Button
+              onClick={() => moreNavigatorHandler(movie.link)}
+              text="WIĘCEJ"
+            ></Button>
+            {/* <MovieCover alt=""/> */}
+            {movie.relatedMovies.map((relatedMovie) => (
+              <MovieCover
+                key={relatedMovie.id}
+                alt={relatedMovie.title}
+                path={relatedMovie.thumbnail}
+                title={relatedMovie.title}
+                year={relatedMovie.year}
+                onClick={() => movieNavigatorHandler(relatedMovie.id)}
+              />
+            ))}
+          </div>
+        )
+      )}
+    </div>
+  );
+};
+
+export default MovieFunFacts;
