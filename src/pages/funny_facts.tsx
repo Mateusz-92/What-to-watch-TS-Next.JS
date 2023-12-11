990f91ac99f1a57acd9c9e1046cbda1ae83a551d
Mateusz-92 <matuesz.zasada92@gmail.com>
2023-12-11 20:08:03 +0100

    added funny_facts route

---
+++ src/pages/funny_facts.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import MovieFunFacts from "@/components/movies_category/movies_genre/MovieFunFacts";
+
+const FunnyFucts: React.FC = () => {
+  return <MovieFunFacts />;
+};
+export default FunnyFucts;
