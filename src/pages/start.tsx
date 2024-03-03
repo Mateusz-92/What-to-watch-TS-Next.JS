e77e7366b210d72e0e5ca7702ea8fda6fbc92d29
Mateusz-92 <matuesz.zasada92@gmail.com>
2024-03-03 17:35:24 +0100

    added item for component

--- src/pages/start.tsx
+++ src/pages/start.tsx
@@ -1,10 +1,43 @@
-import Carousel from "@/components/carousel/Carousel";
+import Carousel, { Item } from "@/components/carousel/Carousel";
 import React from "react";
+const items: Item[] = [
+  {
+    name: "Ranking",
+    redirect: "rank",
+    image: "/images/rank.png",
+  },
+  {
+    name: "Rok produkcji",
+    redirect: "year",
+    image: "/images/year_of_production.png",
+  },
+  {
+    name: "Polecane",
+    redirect: "ourfavourites",
+    image: "/images/recommended.png",
+  },
+
+  {
+    name: "Gatunek",
+    redirect: "genres",
+    image: "/images/genre.png",
+  },
+  {
+    name: "Losowy",
+    redirect: "random",
+    image: "/images/select_tag.png",
+  },
+  {
+    name: "Vod",
+    redirect: "vod",
+    image: "/images/vod.png",
+  },
+];
 
 const Start: React.FC = () => {
   return (
     <>
-      <Carousel />
+      <Carousel items={items} />
     </>
   );
 };
