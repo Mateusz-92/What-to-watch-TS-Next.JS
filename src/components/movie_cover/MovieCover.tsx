19996b412abf77522ba7662f9cacaa034c9c4359
Mateusz-92 <matuesz.zasada92@gmail.com>
2024-03-03 17:32:42 +0100

    destructurated props

--- src/components/movie_cover/MovieCover.tsx
+++ src/components/movie_cover/MovieCover.tsx
@@ -1,28 +1,29 @@
 import React, { useState } from "react";
 import styles from "./MovieCover.module.css";
+import { emptyImagePath } from "@/constants";
 
+const emptyImage: string = emptyImagePath;
 const MovieCover: React.FC<{
   onClick: () => void;
   path: string;
   alt: string;
   title: string;
   year: number;
-}> = (props) => {
-  const [imagePath, setImagePath] = useState<string>(props.path);
-  const emptyImage: string = "/images/empty_image.png";
+}> = ({ path, alt, title, year, onClick }) => {
+  const [imagePath, setImagePath] = useState<string>(path);
 
   const handleImageError = () => {
     setImagePath(emptyImage);
   };
 
   return (
-    <div onClick={props.onClick} className={styles.container}>
+    <div onClick={onClick} className={styles.container}>
       <div className={styles.wrapper}>
         <div>
-          <img src={imagePath} onError={handleImageError} alt={props.alt}></img>
+          <img src={imagePath} onError={handleImageError} alt={alt}></img>
         </div>
-        <span>{props.title}</span>
-        <span>{props.year}</span>
+        <span>{title}</span>
+        <span>{year}</span>
       </div>
     </div>
   );
