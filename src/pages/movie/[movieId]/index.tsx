b69c34a11a3ae00035a2ad9f811b7ced61dea716
Mateusz-92 <matuesz.zasada92@gmail.com>
2024-03-10 11:28:51 +0100

    modificated code to transtack queryFn

--- src/pages/movie/[movieId]/index.tsx
+++ src/pages/movie/[movieId]/index.tsx
@@ -5,18 +5,22 @@ import MovieDetails from "@/components/movie_details/MovieDetails";
 import { getMovieById, getRandomSuggestedMovie } from "../../api/api";
 import { MovieData } from "../../api/api";
 import MoviesCoverList from "@/components/movie_cover/movie_cover_list/MoviesCoverList";
+import { BouncingDotsLoader } from "@/components/common/loader/BouncingDotsLoader";
+import { useGetMovieById } from "@/pages/api/queries";
 const Movie: React.FC = () => {
-  const [movie, setMovie] = useState<MovieData>();
   const router = useRouter();
 
   const movieId = router.query.movieId as string;
-  useEffect(() => {
-    if (movieId) {
-      getMovieById(movieId).then((data) => {
-        setMovie(data);
-      });
-    }
-  }, [movieId]);
+
+  const { data: movie, isLoading, isError } = useGetMovieById(movieId);
+  // const { data } = useGetRandomSuggestedMovie(movie?.id.toString() || "");
+
+  if (isLoading) {
+    return <BouncingDotsLoader />;
+  }
+  if (isError) {
+    return <div>fetched is failed</div>;
+  }
 
   return (
     <div>
@@ -34,8 +38,8 @@ const Movie: React.FC = () => {
             key={movie.id}
           />
           <MoviesCoverList
-            fetch={getRandomSuggestedMovie}
-            tag={movie.id.toString()}
+            getDataFn={getRandomSuggestedMovie}
+            tag={movieId || ""}
           />
         </>
       )}
@@ -44,3 +48,51 @@ const Movie: React.FC = () => {
 };
 
 export default Movie;
+// import React from "react";
+// import { useEffect, useState } from "react";
+// import { useRouter } from "next/router";
+// import MovieDetails from "@/components/movie_details/MovieDetails";
+// import { getMovieById, getRandomSuggestedMovie } from "../../api/api";
+// import { MovieData } from "../../api/api";
+// import MoviesCoverList from "@/components/movie_cover/movie_cover_list/MoviesCoverList";
+// import { BouncingDotsLoader } from "@/components/common/loader/BouncingDotsLoader";
+// const Movie: React.FC = () => {
+//   const [movie, setMovie] = useState<MovieData>();
+//   const router = useRouter();
+
+//   const movieId = router.query.movieId as string;
+//   useEffect(() => {
+//     if (movieId) {
+//       getMovieById(movieId).then((data) => {
+//         setMovie(data);
+//       });
+//     }
+//   }, [movieId]);
+
+//   return (
+//     <div>
+//       {!movie && <BouncingDotsLoader />}
+//       {movie && (
+//         <>
+//           <MovieDetails
+//             title={movie.title}
+//             alt={movie.title}
+//             country={movie.country}
+//             description={movie.description}
+//             path={movie.thumbnail}
+//             year={movie.year}
+//             genres={movie.genres}
+//             vod={movie.vod || []}
+//             key={movie.id}
+//           />
+//           <MoviesCoverList
+//             fetch={getRandomSuggestedMovie}
+//             tag={movie.id.toString()}
+//           />
+//         </>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default Movie;
