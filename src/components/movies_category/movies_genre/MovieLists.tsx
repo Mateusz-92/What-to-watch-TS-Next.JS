afee5d4435a9a6861682e73c4f2bb8b4ba3ee0e9
Mateusz-92 <matuesz.zasada92@gmail.com>
2024-03-10 11:26:05 +0100

    modificated code to transtack queryFn

--- src/components/movies_category/movies_genre/MovieLists.tsx
+++ src/components/movies_category/movies_genre/MovieLists.tsx
@@ -1,26 +1,29 @@
-import {
-  ListData,
-  MovieItem,
-  getLists,
-  getMovieByTag,
-} from "../../../pages/api/api";
-import React, { useState, useEffect } from "react";
-
-// import { MenuData } from "../../common/drop_down_menu/DropDown";
-import styles from "./MovieGenres.module.css";
-
+import { getMovieByTag } from "../../../pages/api/api";
+import React, { useState } from "react";
 import DropDownMenu from "@/components/common/drop_down_menu/DropDown";
 import MoviesCoverList from "@/components/movie_cover/movie_cover_list/MoviesCoverList";
 import CategoryHeader from "@/components/common/headers/CategoryHeader";
+import { useGetLists } from "@/pages/api/queries";
+import { BouncingDotsLoader } from "@/components/common/loader/BouncingDotsLoader";
 
 const MovieLists: React.FC = (props) => {
-
   const [selectedTag, setSelectedTag] = useState<string>("");
 
-
+  const {
+    data: listsData,
+    isError: listsError,
+    isLoading: listsLoading,
+  } = useGetLists();
   const changeTagHandler = (tag: string) => {
     setSelectedTag(tag);
   };
+  if (listsLoading) {
+    return <BouncingDotsLoader />;
+  }
+  if (listsError) {
+    return <div>failed fetched lists list</div>;
+  }
+
   return (
     <div>
       <CategoryHeader
@@ -30,12 +33,12 @@ const MovieLists: React.FC = (props) => {
       />
 
       <DropDownMenu
-        startTitle="Lista"
-        fetchList={getLists}
+        startTitle={selectedTag || "Lista rankingowa"}
         onSelectTag={changeTagHandler}
+        queryData={listsData}
       />
-   
-      <MoviesCoverList tag={selectedTag} fetch={getMovieByTag} />
+
+      <MoviesCoverList tag={selectedTag} getDataFn={getMovieByTag} />
     </div>
   );
 };
