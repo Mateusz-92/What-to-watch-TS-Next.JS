625700e6c0691279b578dc69a93552c25e596322
Mateusz-92 <matuesz.zasada92@gmail.com>
2024-03-10 11:27:00 +0100

    modificated code to transtack query wrapper

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,16 +3,35 @@ import BottomBar from "@/components/bottom_bar/bottomBar";
 import Logo from "@/components/common/headers/Logo";
 
 import "@/styles/globals.css";
+import {
+  QueryCache,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const queryClient = new QueryClient({
+    queryCache: new QueryCache(),
+    defaultOptions: {
+      queries: {
+        gcTime: 60_000,
+      },
+    },
+  });
   return (
     <div>
-      <ComponentWrapper>
-        <Logo />
-        <Component {...pageProps} />
-        <BottomBar />
-      </ComponentWrapper>
+      <QueryClientProvider client={queryClient}>
+        {process.env.NODE_ENV === "development" && (
+          <ReactQueryDevtools initialIsOpen={false} />
+        )}{" "}
+        <ComponentWrapper>
+          <Logo />
+          <Component {...pageProps} />
+          <BottomBar />
+        </ComponentWrapper>
+      </QueryClientProvider>
     </div>
   );
 }
