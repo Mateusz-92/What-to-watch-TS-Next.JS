778b61e517044b9b813baeb34505b7eadbcb7a27
Mateusz-92 <matuesz.zasada92@gmail.com>
2024-03-10 11:25:57 +0100

    modificated code to transtack queryFn

--- src/components/movies_category/movies_genre/MovieGenres.tsx
+++ src/components/movies_category/movies_genre/MovieGenres.tsx
@@ -1,53 +1,44 @@
 import React, { useState, useEffect } from "react";
-import { getGenres, getMovieByTag } from "../../../pages/api/api";
-import { ListData } from "../../../pages/api/api";
-import styles from "./MovieGenres.module.css";
-
+import { getMovieByTag } from "../../../pages/api/api";
 import DropDownMenu from "@/components/common/drop_down_menu/DropDown";
 import MoviesCoverList from "@/components/movie_cover/movie_cover_list/MoviesCoverList";
 import CategoryHeader from "@/components/common/headers/CategoryHeader";
+import { useGetGenres } from "@/pages/api/queries";
+import { BouncingDotsLoader } from "@/components/common/loader/BouncingDotsLoader";
 
-const movies = [
-  {
-    id: 1073,
-    title: "Ziemia obiecana ",
-    year: 1974,
-    thumbnail: "https://fwcdn.pl/fpo/10/73/1073/7998093.3.jpg",
-  },
-  {
-    id: 1158,
-    title: "Krótki film o miłości ",
-    year: 1988,
-    thumbnail: "https://fwcdn.pl/fpo/11/58/1158/7879818.3.jpg",
-  },
-  {
-    id: 4830,
-    title: "Człowiek na torze ",
-    year: 1956,
-    thumbnail: "https://fwcdn.pl/fpo/48/30/4830/7182176.3.jpg",
-  },
-];
-
-const MovieGenres: React.FC = (props) => {
+const MovieGenres: React.FC = () => {
   const [selectedTag, setSelectedTag] = useState<string>("");
+
+  const {
+    data: genresData,
+    isError: genresError,
+    isLoading: genresLoading,
+  } = useGetGenres();
 
   const changeTagHandler = (tag: string) => {
     setSelectedTag(tag);
   };
+  if (genresLoading) {
+    return <BouncingDotsLoader />;
+  }
+  if (genresError) {
+    return <div>failed fetched genres list</div>;
+  }
+
   return (
     <div>
       <CategoryHeader
         title="Gatunki"
-      path="images/genre.png"
+        path="images/genre.png"
         alt="gatunki filmowe"
       />
       <DropDownMenu
-        fetchList={getGenres}
+        queryData={genresData}
         startTitle={selectedTag || "Gatunek"}
         onSelectTag={changeTagHandler}
       />
 
-      <MoviesCoverList tag={selectedTag} fetch={getMovieByTag} />
+      <MoviesCoverList tag={selectedTag} getDataFn={getMovieByTag} />
     </div>
   );
 };
