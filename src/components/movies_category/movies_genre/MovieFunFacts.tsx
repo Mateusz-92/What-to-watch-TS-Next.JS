7c42fb1c622db2928931f9832644cc2d13b627b3
Mateusz-92 <matuesz.zasada92@gmail.com>
2024-03-10 11:25:31 +0100

    modificated code to transtack query

--- src/components/movies_category/movies_genre/MovieFunFacts.tsx
+++ src/components/movies_category/movies_genre/MovieFunFacts.tsx
@@ -1,70 +1,62 @@
-import React, { useEffect, useState, useRef } from "react";
-import MovieDetails from "@/components/movie_details/MovieDetails";
-import { FunFact } from "@/pages/api/api";
-import { fetchRandomFunFact } from "@/pages/api/api";
+import React from "react";
 import { useRouter } from "next/router";
 import Button from "@/components/common/buttons/button/Button";
 import MovieCover from "@/components/movie_cover/MovieCover";
+import MovieDetails from "@/components/movie_details/MovieDetails";
+import { useGetRandomFunFact } from "@/pages/api/queries";
+import { BouncingDotsLoader } from "@/components/common/loader/BouncingDotsLoader";
 
 const MovieFunFacts: React.FC = () => {
-  const [movie, setMovie] = useState<FunFact | null>(null);
-  const [isLoading, setIsLoading] = useState(true);
-  const hasFetchedData = useRef(false);
   const router = useRouter();
+  const { data: movie, isLoading, refetch, isError } = useGetRandomFunFact();
 
   const fetchFanFactsHandler = () => {
-    setIsLoading(true);
-    fetchRandomFunFact().then((data) => {
-      setMovie(data);
-      setIsLoading(false);
-      console.log("fetch fun");
-    });
+    refetch();
+    console.log("fetch fun");
   };
+
   const moreNavigatorHandler = (link: string) => {
     router.push(link);
   };
+
   const movieNavigatorHandler = (id: number) => {
     router.push(`movie/${id}`);
   };
-  useEffect(() => {
-    if (!hasFetchedData.current) {
-      fetchFanFactsHandler();
-      hasFetchedData.current = true;
-    }
-  }, []);
 
+  if (isLoading) {
+    return <BouncingDotsLoader />;
+  }
+  if (isError) {
+    return <div>Error fetching facts</div>;
+  }
   return (
     <div>
-      {isLoading ? (
-        <p>Loading...</p>
-      ) : (
-        movie && (
-          <div className="tmp">
-            <MovieDetails
-              title={movie.title}
-              alt={movie.title}
-              description={movie.content}
-              path={`https://wtwapi.azurewebsites.net${movie.image}`}
-              key={movie.id}
-            />
-            <Button onClick={fetchFanFactsHandler} text="LOSUJ"></Button>
-            <Button
-              onClick={() => moreNavigatorHandler(movie.link)}
-              text="WIĘCEJ"
-            ></Button>
+      {movie && (
+        <div className="tmp">
+          <MovieDetails
+            title={movie.title}
+            alt={movie.title}
+            description={movie.content}
+            path={`https://wtwapi.azurewebsites.net${movie.image}`}
+            key={movie.id}
+          />
+          <Button onClick={fetchFanFactsHandler} text="LOSUJ"></Button>
+          <Button
+            onClick={() => moreNavigatorHandler(movie.link)}
+            text="WIĘCEJ"
+          ></Button>
 
-            {movie.relatedMovies.map((relatedMovie) => (
-              <MovieCover
-                key={relatedMovie.id}
-                alt={relatedMovie.title}
-                path={relatedMovie.thumbnail}
-                title={relatedMovie.title}
-                year={relatedMovie.year}
-                onClick={() => movieNavigatorHandler(relatedMovie.id)}
-              />
-            ))}
-          </div>
-        )
+          {movie.relatedMovies.map((relatedMovie) => (
+            <MovieCover
+              key={relatedMovie.id}
+              alt={relatedMovie.title}
+              path={relatedMovie.thumbnail}
+              title={relatedMovie.title}
+              year={relatedMovie.year}
+              onClick={() => movieNavigatorHandler(relatedMovie.id)}
+            />
+          ))}
+        </div>
       )}
     </div>
   );
