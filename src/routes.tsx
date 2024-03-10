4314638c84aec8339c21ef901de0bd13cbeed3c4
Mateusz-92 <matuesz.zasada92@gmail.com>
2024-03-10 18:58:49 +0100

    deleted slash from routes variables

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,10 +1,10 @@
-export const MOVIE_RANK = "/rank";
-export const MOVIE_YEAR = "/year";
-export const MOVIE_OUR_FAVOURITES = "/ourfavourites";
-export const MOVIE_GENRES = "/genres";
-export const MOVIE_RANDOM = "/random";
-export const MOVIE_VOD = "/vod";
-export const FUNNY_FACTS="/funny_facts";
-export const ABOUT = "/about";
+export const MOVIE_RANK = "rank";
+export const MOVIE_YEAR = "year";
+export const MOVIE_OUR_FAVOURITES = "ourfavourites";
+export const MOVIE_GENRES = "genres";
+export const MOVIE_RANDOM = "random";
+export const MOVIE_VOD = "vod";
+export const FUNNY_FACTS = "/funny_facts";
+export const ABOUT = "about";
 
-export const MOVIE ="/movie"
+export const MOVIE = "movie";
